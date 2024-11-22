1a68deea3ee15869ee799ab72d1e0edae7b8808d
Thomas Schmid <tschmid35@gmail.com>
2024-11-22 20:25:54 +0100

    Update ThemeActivityViewModel.kt

--- app/src/main/java/moe/smoothie/androidide/themestore/viewmodels/ThemeActivityViewModel.kt
+++ app/src/main/java/moe/smoothie/androidide/themestore/viewmodels/ThemeActivityViewModel.kt
@@ -11,16 +11,15 @@ import moe.smoothie.androidide.themestore.ThemeState
 import okhttp3.OkHttpClient
 import javax.inject.Inject
 
-@AssistedFactory
-interface ThemeActivityViewModelFactory {
-    fun create(url: String)
-}
-
 @HiltViewModel
 class ThemeActivityViewModel @Inject constructor(
     private val httpClient: OkHttpClient,
     @Assisted private val url: String
 ) : ViewModel() {
+    interface ThemeActivityViewModelFactory {
+        fun create(url: String)
+    }
+
     private val _isLoading = MutableStateFlow(false)
     val isLoading: StateFlow<Boolean> = _isLoading
 
@@ -28,6 +27,6 @@ class ThemeActivityViewModel @Inject constructor(
     val themeState: StateFlow<ThemeState?> = _themeState
 
     fun loadInfo() {
-
+        
     }
 }
